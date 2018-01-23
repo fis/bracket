56bc2a6ccb7409d4e5dec9ee007b0406cd3862a1
Heikki Kallasjoki <fis@zem.fi>
2018-01-23 03:29:53 +0000

    Remove dynamic module loading for unnecessary complexity.

--- irc/bot/bot.cc
+++ irc/bot/bot.cc
@@ -3,28 +3,52 @@
 #include "base/exc.h"
 #include "base/log.h"
 #include "irc/bot/bot.h"
-#include "irc/bot/config.pb.h"
-
-extern "C" {
-#include <dlfcn.h>
-}
 
 namespace irc::bot {
 
-Bot::Bot(const std::function<std::unique_ptr<google::protobuf::Message>()>& config_loader, event::Loop* loop)
-    : config_loader_(config_loader)
-{
-  if (loop) {
-    loop_ = loop;
+void Bot::Run() {
+  // process the configuration proto and create plugins
+
+  auto config = LoadConfig();
+
+  const irc::Config* irc_config = nullptr;
+
+  if (config->GetDescriptor()->full_name() == irc::Config::descriptor()->full_name()) {
+    irc_config = static_cast<const irc::Config*>(config.get());
   } else {
-    private_loop_ = std::make_unique<event::Loop>();
-    loop_ = private_loop_.get();
+    std::vector<const google::protobuf::FieldDescriptor*> fields;
+    auto reflection = config->GetReflection();
+    reflection->ListFields(*config, &fields);
+
+    for (const auto* field : fields) {
+      if (field->type() != google::protobuf::FieldDescriptor::TYPE_MESSAGE)
+        continue;
+
+      if (field->message_type()->full_name() == irc::Config::descriptor()->full_name()) {
+        CHECK(!field->is_repeated());
+        irc_config = static_cast<const irc::Config*>(&reflection->GetMessage(*config, field));
+      } else {
+        auto factory = plugin_registry_.find(field->message_type()->full_name());
+        if (factory == plugin_registry_.end())
+          continue;
+        if (field->is_repeated()) {
+          FATAL("TODO: repeated plugin fields");
+        } else {
+          LOG(INFO) << "Creating plugin for: " << field->message_type()->full_name();
+          auto plugin = factory->second(reflection->GetMessage(*config, field), this);
+          plugins_.push_back(std::move(plugin));
+        }
+      }
+    }
   }
 
-  Load();
-}
+  // start the IRC connection and run
 
-void Bot::Run() {
+  if (!irc_config)
+    throw base::Exception("could not find IRC connection config field");
+
+  irc_ = std::make_unique<irc::Connection>(*irc_config, loop_);
+  irc_->AddReader(this);
   irc_->Start();
   while (true)
     loop_->Poll();
@@ -48,90 +72,12 @@ void Bot::MessageReceived(const irc::Message& msg) {
   LOG(DEBUG) << debug;
 }
 
-void Bot::Load() {
-  auto config = config_loader_();
-
-  // locate the basic IRC connection and bot config elements
-
-  const irc::Config* irc_config = nullptr;
-  const BotConfig* bot_config = nullptr;
-
-  if (config->GetDescriptor()->full_name() == irc::Config::descriptor()->full_name()) {
-    irc_config = static_cast<const irc::Config*>(config.get());
-  } else if (config->GetDescriptor()->full_name() == BotConfig::descriptor()->full_name()) {
-    bot_config = static_cast<const BotConfig*>(config.get());
+Bot::Bot(event::Loop* loop) {
+  if (loop) {
+    loop_ = loop;
   } else {
-    std::vector<const google::protobuf::FieldDescriptor*> fields;
-    auto reflection = config->GetReflection();
-    reflection->ListFields(*config, &fields);
-
-    for (const auto* field : fields) {
-      if (field->type() != google::protobuf::FieldDescriptor::TYPE_MESSAGE || field->is_repeated())
-        continue;
-      if (field->message_type()->full_name() == irc::Config::descriptor()->full_name())
-        irc_config = static_cast<const irc::Config*>(&reflection->GetMessage(*config, field));
-      else if (field->message_type()->full_name() == BotConfig::descriptor()->full_name())
-        bot_config = static_cast<const BotConfig*>(&reflection->GetMessage(*config, field));
-    }
-  }
-
-  if (!irc_config && bot_config && bot_config->has_irc())
-    irc_config = &bot_config->irc();
-
-  // create the IRC connection, if this is the first load
-
-  if (!irc_) {
-    if (!irc_config)
-      throw base::Exception("could not find IRC connection config field");
-    irc_ = std::make_unique<irc::Connection>(*irc_config, loop_);
-    irc_->AddReader(this);
-  }
-
-  // load any dynamic modules
-
-  if (bot_config && bot_config->modules_size()) {
-    for (const auto& module_file : bot_config->modules()) {
-      LOG(INFO) << "Loading module: " << module_file;
-
-      void* module_handle = dlopen(module_file.c_str(), RTLD_LAZY | RTLD_LOCAL);
-      if (!module_handle)
-        throw base::Exception("dlopen", errno);
-
-      auto module_init = (void (*)(const internal::PluginRegistrar&)) dlsym(module_handle, "irc_bot_module_init");
-      if (!module_init) {
-        dlclose(module_handle);
-        throw base::Exception("no initialization function in module");
-      }
-
-      module_handles_.push_back(module_handle);
-      module_init(
-          [this](auto desc, auto create, auto destroy) {
-            plugin_registry_.try_emplace(desc->full_name(), create, destroy);
-          });
-    }
-  }
-
-  // create all configured plugins
-
-  {
-    std::vector<const google::protobuf::FieldDescriptor*> fields;
-    auto reflection = config->GetReflection();
-    reflection->ListFields(*config, &fields);
-
-    for (const auto* field : fields) {
-      if (field->type() != google::protobuf::FieldDescriptor::TYPE_MESSAGE)
-        continue;
-
-      auto plugin_factory = plugin_registry_.find(field->message_type()->full_name());
-      if (plugin_factory == plugin_registry_.end())
-        continue;
-
-      LOG(INFO) << "Creating plugin: " << field->message_type()->full_name();
-
-      plugins_.emplace_back(
-          plugin_factory->second.first(reflection->GetMessage(*config, field), this),
-          plugin_factory->second.second);
-    }
+    private_loop_ = std::make_unique<event::Loop>();
+    loop_ = private_loop_.get();
   }
 }
 
--- irc/bot/bot.h
+++ irc/bot/bot.h
@@ -16,34 +16,60 @@
 #include "irc/connection.h"
 #include "irc/message.h"
 #include "irc/bot/plugin.h"
+#include "proto/util.h"
 
 namespace irc::bot {
 
 class Bot : public PluginHost, public irc::Connection::Reader {
  public:
-  Bot(const std::function<std::unique_ptr<google::protobuf::Message>()>& config_loader, event::Loop* loop = nullptr);
-
   void Run();
 
   void Send(const Message& msg) override;
-
   void MessageReceived(const irc::Message& msg) override;
 
   event::Loop* loop() override { return loop_; }
 
- private:
-  std::function<std::unique_ptr<google::protobuf::Message>()> config_loader_;
+  virtual ~Bot() = default;
 
-  std::vector<void*> module_handles_;
-  std::unordered_map<std::string, std::pair<internal::PluginCreator*, internal::PluginDestroyer*>> plugin_registry_;
-  std::vector<std::unique_ptr<Plugin, internal::PluginDestroyer*>> plugins_;
+ protected:
+  explicit Bot(event::Loop* loop = nullptr);
+
+  template <typename PluginConfigProto, typename PluginClass>
+  void RegisterPlugin() {
+    plugin_registry_.try_emplace(
+        PluginConfigProto::descriptor()->full_name(),
+        [](const google::protobuf::Message& config, PluginHost* host) -> std::unique_ptr<Plugin> {
+          return std::make_unique<PluginClass>(static_cast<const PluginConfigProto&>(config), host);
+        });
+  }
+
+  virtual std::unique_ptr<google::protobuf::Message> LoadConfig() = 0;
+
+ private:
+  using PluginFactory = std::function<std::unique_ptr<Plugin>(const google::protobuf::Message& config, PluginHost* host)>;
+  std::unordered_map<std::string, PluginFactory> plugin_registry_;
 
   event::Loop* loop_;
   std::unique_ptr<event::Loop> private_loop_;
 
+  std::vector<std::unique_ptr<Plugin>> plugins_;
   std::unique_ptr<irc::Connection> irc_;
+};
 
-  void Load();
+template <typename ConfigProto>
+class ConfiguredBot : public Bot {
+ public:
+  ConfiguredBot(const char* config_file) : config_file_(config_file) {}
+
+ protected:
+  std::unique_ptr<google::protobuf::Message> LoadConfig() override {
+    auto config = std::make_unique<ConfigProto>();
+    proto::ReadText(config_file_, config.get());
+    return config;
+  }
+
+ private:
+  const char* config_file_;
 };
 
 } // namespace irc::bot
